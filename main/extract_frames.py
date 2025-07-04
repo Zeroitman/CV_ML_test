b6df690b77e115fac07395af32811b207d05c7b7
borsh130894 <borsh130894@gmail.com>
2025-07-04 17:41:04 +0600

    Added extract_frames module

---
+++ main/extract_frames.py
@@ -0,0 +1,35 @@
+import os
+import subprocess
+from .main import FRAMES_PATH, VIDEO_PATH
+
+
+def extract_frames(file_name, fps=1):
+    video_file = os.path.join(VIDEO_PATH, file_name)
+    output_path = os.path.join(FRAMES_PATH, file_name)
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    output_pattern = os.path.join(output_path, "frame_%05d.png")
+
+    command = [
+        'ffmpeg',
+        '-i', video_file,
+        '-vf', f'fps={fps}',
+        output_pattern
+    ]
+
+    try:
+        subprocess.run(command, check=True)
+        print(f"Кадры успешно извлечены в {FRAMES_PATH} с частотой {fps} FPS.")
+    except subprocess.CalledProcessError as e:
+        print(f"Ошибка при извлечении кадров: {e}")
+    except FileNotFoundError:
+        print("Ошибка: FFmpeg не найден.")
+
+
+def main():
+    file_name = "2_1.MOV"
+    extract_frames(file_name, fps=1)
+
+if __name__ == "__main__":
+    main()
